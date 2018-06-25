3cc5d31761167eb1c836c5f2aeb5d18845113ba0
biddyweb <webframes@gmail.com>
2018-06-25 16:20:54 +0100

    first commit

--- src/perfectcoind.cpp
+++ src/perfectcoind.cpp
@@ -31,8 +31,8 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called Pigeon (https://www.pigeon.org/),
- * which enables instant payments to anyone, anywhere in the world. Pigeon uses peer-to-peer technology to operate
+ * This is the developer documentation of the reference client for an experimental new digital currency called PerfectCoin (https://www.pigeon.org/),
+ * which enables instant payments to anyone, anywhere in the world. PerfectCoin uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
  * The software is a community-driven open source project, released under the MIT license.
@@ -148,7 +148,7 @@ bool AppInit(int argc, char* argv[])
         if (gArgs.GetBoolArg("-daemon", false))
         {
 #if HAVE_DECL_DAEMON
-            fprintf(stdout, "Pigeon server starting\n");
+            fprintf(stdout, "PerfectCoin server starting\n");
 
             // Daemonize
             if (daemon(1, 0)) { // don't chdir (1), do close FDs (0)
